588d420189ee974ad27eb878e4db62f45e6d75ae
pcProgramate <paolaroa1925@gmail.com>
2020-12-17 18:15:28 -0500

    comment

--- assets/menuscroll.js
+++ assets/menuscroll.js
@@ -12,16 +12,13 @@ function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("menu-scroll");
     logo.innerHTML = '<img class="logo" src="images/logo.png" alt="" />';
-    /*for (i=0; i< bars.length;i++){
-      bars[i].classList.add("progress")
-    }
-*/
+
   }
    else {
     header.classList.remove("menu-scroll");
     logo.innerHTML = '<img class="logo" src="images/logoPR.png" alt="" />';
   }
-
+/*aca le digo que si el scroll es mayor a la altura del header me agregue la clase progress que tiene la animacin*/
   if (window.pageYOffset > header.scrollHeight){
     for (i=0; i< bars.length;i++){
       bars[i].classList.add("progress")
