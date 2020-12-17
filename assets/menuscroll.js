1323d2ce1f36f80d4ab024113ac1f586b1fe0a46
pcProgramate <paolaroa1925@gmail.com>
2020-12-17 18:13:45 -0500

    fix animation on progress bar

--- assets/menuscroll.js
+++ assets/menuscroll.js
@@ -5,15 +5,27 @@ window.onscroll = function () {
 var header = document.getElementById("header");
 var logo = document.getElementById("logo-menu");
 var sticky = header.offsetTop;
+var bars = document.getElementsByClassName("bar")
+var skill= document.getElementById("about-me");
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("menu-scroll");
     logo.innerHTML = '<img class="logo" src="images/logo.png" alt="" />';
-  } else {
+    /*for (i=0; i< bars.length;i++){
+      bars[i].classList.add("progress")
+    }
+*/
+  }
+   else {
     header.classList.remove("menu-scroll");
     logo.innerHTML = '<img class="logo" src="images/logoPR.png" alt="" />';
   }
+
+  if (window.pageYOffset > header.scrollHeight){
+    for (i=0; i< bars.length;i++){
+      bars[i].classList.add("progress")
+    }
+  }
 }
 
-//ensayito de las barras de progreso
