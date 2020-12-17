82b99ed6056f72641572ae4d2237f77fd95549c1
pcProgramate <paolaroa1925@gmail.com>
2020-12-17 15:51:45 -0500

    add styles on menu, change prfile pic

--- assets/menuscroll.js
+++ assets/menuscroll.js
@@ -1,12 +1,19 @@
-window.onscroll = function() {myFunction()};
-  
-  var header = document.getElementById("header");
-  var sticky = header.offsetTop;
-  
-  function myFunction() {
-    if (window.pageYOffset > sticky) {
-      header.classList.add("menu-scroll");
-    } else {
-      header.classList.remove("menu-scroll");
-    }
+window.onscroll = function () {
+  myFunction();
+};
+
+var header = document.getElementById("header");
+var logo = document.getElementById("logo-menu");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("menu-scroll");
+    logo.innerHTML = '<img class="logo" src="images/logo.png" alt="" />';
+  } else {
+    header.classList.remove("menu-scroll");
+    logo.innerHTML = '<img class="logo" src="images/logoPR.png" alt="" />';
   }
+}
+
+//ensayito de las barras de progreso
